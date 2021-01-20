5e344c4f19251243d3e366785613e6456d500d5e
Jonathan <coleji@bc.edu>
2021-01-20 13:19:57 -0500

    tweaks, include timestamp with reading, change send dest to ip

--- read.py
+++ read.py
@@ -2,18 +2,20 @@
 
 import sys
 import Adafruit_DHT
+import time
+
 
 def read():
 
-    humidity, temp_c = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 2)
+	humidity, temp_c = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 2)
 
-    temp_f = (temp_c * 1.8) + 32
+	temp_f = (temp_c * 1.8) + 32
 
-    return [humidity, temp_f]
+	return [int(round(time.time())), humidity, temp_f]
+
 
 read()
 
-humidity, temp = read()
+timestamp, humidity, temp = read()
 
-print 'Temp: {0:0.1f}F, Humidity: {1:0.1f}%'.format(temp, humidity)
-
+print 'Time: {2:d}: Temp: {0:0.1f}F, Humidity: {1:0.1f}%'.format(temp, humidity, timestamp)
--- send.py
+++ send.py
@@ -5,14 +5,15 @@ import urllib2
 from read import read
 
 
-humidity, temp = read()
+timestamp, humidity, temp = read()
 
 data = {
-        'temperature': round(temp, 3),
-	'humidity': round(humidity, 3)
+	'temperature': round(temp, 3),
+	'humidity': round(humidity, 3),
+	'timestamp': timestamp
 }
 
-req = urllib2.Request('http://hicks.steingard.lan:8080/log')
+req = urllib2.Request('http://192.168.1.15:8080/log')
 req.add_header('Content-Type', 'application/json')
 
 response = urllib2.urlopen(req, json.dumps(data))
